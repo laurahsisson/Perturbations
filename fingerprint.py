236115dd8b5a773f7beae54db21121a176567104
laurahsisson <laura.h.sisson@gmail.com>
2023-09-24 14:08:33 -0400

    Fingerprint is not gooooood

--- fingerprint.py
+++ fingerprint.py
@@ -8,7 +8,11 @@ import numpy as np
 import sklearn
 import sklearn.linear_model
 import sklearn.kernel_ridge
+import sklearn.decomposition
 import submission
+import smiles
+import mrrmse
+import torch
 
 import matplotlib.pyplot as plt
 import sklearn.metrics
@@ -17,49 +21,62 @@ import sklearn.metrics
 def to_numpy(fp):
     return np.frombuffer(bytes(fp.ToBitString(), 'utf-8'), 'u1') - ord('0')
 
-
-def get_fingerprint(smiles):
-    mol = rdkit.Chem.MolFromSmiles(smiles)
-    fp = mfpgen.GetFingerprint(mol)
-    return to_numpy(fp)
-
-
-def convert_to_input(x_entry):
-    cell_type, smiles = x_entry
-    ct = celltype.one_hot(cell_type)
-    mfp = get_fingerprint(smiles)
-    return np.concatenate([ct, mfp])
+def convert_to_input(fpfn,x_entry):
+    ct, smls = x_entry
+    ct = celltype.one_hot(ct)
+    mfp = fpfn(smls)
+    return mfp#np.concatenate([ct, mfp])
 
 def weight(x_entry):
-    cell_type, smiles = x_entry
-    if celltype.is_evaluation(cell_type):
+    ct, smiles = x_entry
+    if celltype.is_evaluation(ct):
         return 1000
     return 1
 
-def score(y,pred):
-    return sklearn.metrics.mean_squared_error(y,pred,squared=False)
+def model(fname,fpfn):
+    train_x_entries, train_y = data.get_train(data.Include.All)
+    eval_x_entries, eval_y = data.get_train(data.Include.Evaluation)
+            
+    train_x = np.stack([convert_to_input(fpfn,x_entry) for x_entry in train_x_entries])
+    train_weights = np.stack([weight(x_entry) for x_entry in train_x_entries])
+    train_y = np.stack(train_y)
+    decomposer = sklearn.decomposition.TruncatedSVD(n_components=5000).fit(train_y)
 
-train_x_entries, train_y = data.get_train(data.Include.All)
-eval_x_entries, eval_y = data.get_train(data.Include.Evaluation)
-        
-mfpgen = rdkit.Chem.rdFingerprintGenerator.GetMorganGenerator(radius=4,
-                                                      fpSize=2048)
-train_x = np.stack([convert_to_input(x_entry) for x_entry in train_x_entries])
-train_weights = np.stack([weight(x_entry) for x_entry in train_x_entries])
-train_y = np.stack(train_y)
+    eval_x = np.stack([convert_to_input(fpfn,x_entry) for x_entry in eval_x_entries])
+    eval_y = np.stack(eval_y)
 
-eval_x = np.stack([convert_to_input(x_entry) for x_entry in eval_x_entries])
-eval_y = np.stack(eval_y)
+    print("Training model.")
+    model = sklearn.kernel_ridge.KernelRidge(kernel='linear').fit(train_x, decomposer.transform(train_y),sample_weight=train_weights)
 
-# print("Training model.")
-model = sklearn.kernel_ridge.KernelRidge(kernel='linear').fit(train_x, train_y,sample_weight=train_weights)
+    train_pred = decomposer.inverse_transform(model.predict(train_x))
+    eval_pred = decomposer.inverse_transform(model.predict(eval_x))
+    print("Eval",mrrmse.vectorized(torch.tensor(eval_pred),torch.tensor(eval_y)))
 
-train_pred = model.predict(train_x)
-eval_pred = model.predict(eval_x)
-print("Eval",score(eval_y,eval_pred))
+    test_x_entries = data.get_test()
+    test_x = np.stack([convert_to_input(fpfn,x_entry) for x_entry in test_x_entries])
+    test_pred = decomposer.inverse_transform(model.predict(test_x))
+    print(f"Made predictions of shape {test_pred.shape}.")
+    bins = np.linspace(-10, 10, 100)
 
-test_x_entries = data.get_test()
-test_x = np.stack([convert_to_input(x_entry) for x_entry in test_x_entries])
-test_pred = model.predict(test_x)
-print(f"Made predictions of shape {test_pred.shape}.")
-submission.make("fingerprint", test_pred)
+    plt.hist(eval_pred.flatten(), bins, alpha=0.5, label='pred')
+    plt.hist(eval_y.flatten(), bins, alpha=0.5, label='actual')
+    plt.legend(loc='upper right')
+    plt.show()
+    # submission.make(fname, test_pred)
+
+
+def fingerprint():
+    mfpgen = rdkit.Chem.rdFingerprintGenerator.GetMorganGenerator(radius=4,
+                                                          fpSize=128)
+    # Closure city
+    def get_fingerprint(smls):
+        mol = rdkit.Chem.MolFromSmiles(smls)
+        fp = mfpgen.GetFingerprint(mol)
+        return to_numpy(fp)
+
+    model("fingerprint",get_fingerprint)
+
+def onehot():
+    model("onehot",smiles.one_hot)
+
+fingerprint()
--- smiles.py
+++ smiles.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import json
+import torch
 
 fname = 'smiles.json'
 
 mapping = dict()
-
+all_smiles = list()
 
 def build():
     global mapping
@@ -18,11 +19,13 @@ def build():
 
 
 def load():
-    global mapping
+    global mapping, all_smiles
 
     print("Loading smiles from disk.")
     with open(fname, 'r') as f:
         mapping = json.load(f)
+
+    all_smiles = list(sorted(mapping.values()))
 
 
 def get(sm_name):
@@ -30,6 +33,13 @@ def get(sm_name):
         load()
 
     return mapping[sm_name]
+
+def one_hot(sm_name):
+    if not mapping:
+        load()
+
+    idx = torch.tensor(all_smiles.index(sm_name))
+    return torch.nn.functional.one_hot(idx, num_classes=len(all_smiles))
 
 
 if __name__ == "__main__":
